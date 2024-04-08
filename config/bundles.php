bf0bff91280d5a018d78e369477323a20ec342c9
Boris Medvedev <borian.medvedev@gmail.com>
2024-04-08 21:50:23 +0200

    Fixtures install + doctrine downgrade

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,6 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Liip\TestFixturesBundle\LiipTestFixturesBundle::class => ['test' => true],
 ];
